f41da3d82dbd450030e7043da4b6cdbeaba0ea6f
kib <kods9852@gmail.com>
2024-12-16 10:48:44 +0900

    ajax채팅

--- src/main/java/com/metacoding/refsocket/chat/ChatController.java
+++ src/main/java/com/metacoding/refsocket/chat/ChatController.java
@@ -7,6 +7,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @RequiredArgsConstructor
 @Controller
 public class ChatController {
@@ -22,6 +24,12 @@ public class ChatController {
     public String index(Model model) {
         model.addAttribute("models",chatService.findAll());
         return "index";
+    }
+
+    @GetMapping("/api")
+    public ResponseEntity<?> api(){
+        List<Chat> models = chatService.findAll();
+        return ResponseEntity.ok(models);
     }
 
     @PostMapping("/chat")
